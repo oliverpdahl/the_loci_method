373d664485197b08f73853707a3de32680e3057e
Oliver Dahl <oliverpdahl@gmail.com>
2021-04-06 18:20:06 +0000

    Loading and creating journey

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,21 @@ import routes from './routes'
 import Wrapper from '../components/Wrapper'
 import AppBar from '../components/AppBar'
 import { useForm } from 'react-hook-form'
+import { useState } from 'react'
+import firebase from '../firebase'
 import formErrorMessages from '../utils/formErrorMessages'
 
 const Home = () => {
   const { register, errors, handleSubmit, reset } = useForm<{ name: string }>()
+  const [title, setTitle] = useState('')
+  const journeyRef = firebase.database().ref('Journey')
+
+  var user = firebase.auth().currentUser
 
   return (
     <>
       <AppBar
-        title='fullstrapp'
+        title='The Loci Method'
         actions={
           <Button
             color='primary'
@@ -21,7 +27,7 @@ const Home = () => {
             to={routes.signin}
             variant='contained'
           >
-            Sign In
+            Sign In or Sign Up
           </Button>
         }
       />
@@ -30,30 +36,53 @@ const Home = () => {
         <Typography paragraph variant='h3'>
           Create Mind Palaces to Remember Like Never Before
         </Typography>
-        <Typography paragraph>
+        <Typography paragraph variant='h6'>
           An ideal mind palace is a brightly lit deserted place with a standard
           rout through it like a museum or school after hours and is filled with
           striking images of people doing an action to encode information with a
           key transition at every fifth image
         </Typography>
-        <Typography paragraph variant='h5'>
-          Don't forget to configure your firebase settings in{' '}
-          <code>/src/firebase/firebase.ts</code>
+        <Typography paragraph>
+          Key Vocabulary:
+          <br />
+          Image - The visual representation of what you are trying to remember
+          <br />
+          Mind Palace - The visualization or a real or imaginary place where you
+          store images
+          <br />
+          Journey - The mental walk you take through the images you have
+          collected in the mind palace
         </Typography>
-        <Box mt={6}>
-          <Typography paragraph>
-            This is an example form using react-hook-form
-          </Typography>
-        </Box>
+        <Typography paragraph variant='h5'>
+          Create a Journey
+        </Typography>
+
         <form
           onSubmit={handleSubmit(vals => {
-            console.log(vals)
+            if (user) {
+              vals.push({
+                key: 'userID',
+                value: firebase.auth().currentUser?.uid
+              })
+            }
+            journeyRef.push(vals)
             reset()
           })}
         >
           <TextField
-            label='Enter your name'
-            name='name'
+            label='Enter the title of your journey (what are you looking to remember?)'
+            name='title'
+            variant='outlined'
+            fullWidth
+            inputRef={register({
+              required: formErrorMessages.required
+            })}
+            error={!!errors.name}
+            helperText={errors.name?.message || ' '}
+          />
+          <TextField
+            label='What is the location of the mind palace for your journey?'
+            name='location'
             variant='outlined'
             fullWidth
             inputRef={register({
@@ -66,7 +95,6 @@ const Home = () => {
             Submit
           </Button>
         </form>
-        */
       </Wrapper>
     </>
   )
